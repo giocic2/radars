7508ca7c7dca87a5e079af17c22b45fe7a177b5f
giocic2 <74670040+giocic2@users.noreply.github.com>
2022-11-09 15:47:58 +0100

    work in progress

--- custom_modules/picoscope.py
+++ custom_modules/picoscope.py
@@ -16,28 +16,6 @@ def invalid_channel_range():
 def get_channel_range_id(channelRange: str):
     channelRanges = {'20e-3': 1, '50e-3': 2, '100e-3': 3, '200e-3': 4, '500e-3': 5, '1': 6, '2': 7, '5': 8, '10': 9, '20': 10}
     channel_range_id = channelRanges.get(channelRange, invalid_channel_range)
-    # if channelRange == '20e-3':
-    #     channel_range_id = 1
-    # elif channelRange == '50e-3':
-    #     channel_range_id = 2
-    # elif channelRange == '100e-3':
-    #     channel_range_id = 3
-    # elif channelRange == '200e-3':
-    #     channel_range_id = 4
-    # elif channelRange == '500e-3':
-    #     channel_range_id = 5
-    # elif channelRange == '1':
-    #     channel_range_id = 6
-    # elif channelRange == '2':
-    #     channel_range_id = 7
-    # elif channelRange == '5':
-    #     channel_range_id = 8
-    # elif channelRange == '10':
-    #     channel_range_id = 9
-    # elif channelRange == '20':
-    #     channel_range_id = 10
-    # else:
-    #     raise ValueError("The channel range specified in settings.json is not valid.")
     return channel_range_id    
 
 if __name__ == "__main__":
